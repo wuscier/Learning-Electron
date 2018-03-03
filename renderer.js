0f79c25b2922f6baa52d790e79d11e785447cc40
wuscier <1352288123@qq.com>
2018-03-03 14:11:32 +0800

    initial commit

---
+++ renderer.js
@@ -0,0 +1,19 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+const ipc = require('electron').ipcRenderer
+
+const syncMsgBtn = document.getElementById('sendSyncMsgBtn')
+
+syncMsgBtn.addEventListener('click', function () {
+	const reply = ipc.sendSync('synchronous-message', 'Mr. Watson, come here.')
+		console.log(reply)
+		const message =  `Synchronous message reply: ${reply}`
+		document.getElementById('syncReply').innerHTML = message
+})
+
+const asyncMsgBtn = document.getElementById('sendAsyncMsgBtn')
+
+asyncMsgBtn.addEventListener('click', function(){
+	ipc.send('')
+})
